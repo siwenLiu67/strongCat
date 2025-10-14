84567bc0d4b1a72183de1e2aa04d4e505953d99c
siwen <liusiwen67@126.com>
2025-10-14 19:08:26 +0800

    1

--- icot_code_2/experiments/run_fjsp_comparison.py
+++ icot_code_2/experiments/run_fjsp_comparison.py
@@ -34,7 +34,7 @@ def get_all_agents(env):
     """
     return {
         "DQN": D3QN_Agent(env, config.DQN_CONFIG),
-     #  "GA": GA_Agent(env, config.GA_CONFIG),
+         "GA": GA_Agent(env, config.GA_CONFIG),
       #  "SA": SA_Agent(env, config.SA_CONFIG),
       #  "PSO": PSO_Agent(env, config.PSO_CONFIG),
       #  "A2C": A2C_Agent(env, config.A2C_CONFIG),
